5a946296216f1089b42bf9e1b9b183bba75d0898
정영호 <156313348+gggrhrh@users.noreply.github.com>
2024-02-27 13:50:18 +0900

    로비 및 마우스 포인터

---
+++ Assets/Scripts/MousePanelCtrl.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MousePanelCtrl : MonoBehaviour
+{
+    Vector3 MousePos = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 MousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f);
+
+        if (MousePos.x < 0.0f)
+            MousePos.x = 0.0f;
+        if (MousePos.x > Screen.width)
+            MousePos.x = Screen.width;
+        if (MousePos.y < 0.0f)
+            MousePos.y = 0.0f;
+        if (MousePos.y > Screen.height)
+            MousePos.y = Screen.height;
+
+        transform.position = MousePos;
+    }
+
+    void MouseClick()
+    {
+
+    }
+}
