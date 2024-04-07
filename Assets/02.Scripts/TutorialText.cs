1e35ad7281c7eb8dce0ef453bc2ac42cbcfa3ea6
정영호 <gggrhrh98@naver.com>
2024-04-07 16:45:55 +0900

    튜토리얼 텍스트

--- Assets/02.Scripts/TutorialText.cs
+++ Assets/02.Scripts/TutorialText.cs
@@ -2,13 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TutorialText
+public class TutorialText : MonoBehaviour
 {
     public Dictionary<string, string> m_Tutorial = new Dictionary<string, string>();
+
     public void SetText()
     {
         m_Tutorial.Add("Spawn", "우선 숫자 캐릭터를 소환해 볼까요?");
         m_Tutorial.Add("Spawn", "소환은 이 버튼으로 소환이 가능합니다.");
         m_Tutorial.Add("Spawn", "숫자 캐릭터를 소환할때 100골드씩 소모됩니다.");
+    }
+
+    private void Start()
+    {
+        SetText();
+    }
+
+    private void Update()
+    {
+        
     }
 }
