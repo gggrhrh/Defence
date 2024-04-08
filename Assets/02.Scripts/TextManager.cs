e60e0830fa6fd0179501a939fcd0fdf3463ebd5d
정영호 <gggrhrh98@naver.com>
2024-04-08 19:47:06 +0900

    튜토리얼 진행

---
+++ Assets/02.Scripts/TextManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextManager : MonoBehaviour
+{
+    Dictionary<string, string[]> m_Tutorial; //튜토리얼 종류와 대화배열
+
+    void Awake()
+    {
+        m_Tutorial = new Dictionary<string, string[]>();
+        GenerateDate();
+    }
+
+    void GenerateDate()
+    {
+        m_Tutorial.Add("소환하기", new string[] { "우선 숫자 캐릭터를 소환해 볼까요?",
+                        "소환을 하기 위해서는 이 버튼을 눌러야합니다.",
+                        "소환비용은 100골드에요. 총 5번 소환해봅시다."});
+
+        m_Tutorial.Add("조합하기", new string[] { "이번에는 숫자 캐릭터들을 조합해볼거에요",
+                        "조합은 숫자캐릭터를 드래그하여 다른 숫자캐릭터와 조합하면 됩니다.",
+                        "같은 레벨과 같은 타입의 숫자끼리만 조합이 가능하니까 주의하세요~."});
+
+    }
+
+    public string GetText(string Tutorial, int TextIndex)
+    {
+        if (TextIndex == m_Tutorial[Tutorial].Length)
+            return null;
+
+        return m_Tutorial[Tutorial][TextIndex];
+    }
+}
--- Assets/02.Scripts/TutorialMgr.cs
+++ Assets/02.Scripts/TutorialMgr.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TutorialMgr : MonoBehaviour
 {
+    public GameObject TutorialPanel = null;
+    public Text m_PanelText = null;
+    public TextManager textManager;
+
+    int TextIndex = 0;
+    bool IsPanel = false;
+
+    [Header("------ SpawnTutorial ------")]
+    public GameObject SpawnPanel = null;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +25,19 @@ public class TutorialMgr : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void Text(string Tutorial)
+    {
+        string a_Text = textManager.GetText(Tutorial, TextIndex);
+
+        if (a_Text == null)
+        { 
+            IsPanel = false;
+            return;
+        }
+
+        m_PanelText.text = a_Text;
     }
 
 }
